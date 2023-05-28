bb2fb0b5725f19def15332be0cb6e32d8aefa84f
jackson.gao <jackson.gao@atomintl.com>
2023-05-28 22:27:11 +0800

    feat: add github link

--- README.md
+++ README.md
@@ -1,6 +1,3 @@
-### 项目创建和运行
-1. npm install create-react-app -g
-2. create-react-app your-project-name --template typescript
-3. 调出默认的 webpack 配置文件，运行 npm run eject
-4. npm run start 运行项目
+### 安装依赖及运行项目
 
+yarn && yarn start
--- src/pages/layout/index.tsx
+++ src/pages/layout/index.tsx
@@ -1,70 +1,83 @@
-import { ReactNode, useMemo, useState } from 'react';
-import styles from './styles.module.css';
-import { Radio, Switch } from 'antd';
-import { useTheme } from '@hooks';
-import ThreeCuteBrothers from '../threeCuteBrothers';
-import VisualSortingAlgorithm from '../visualSortingAlgorithm';
-import PeriodicTable from '../periodicTable';
-import GlowingBall from '../glowingBall';
+import { ReactNode, useMemo, useState } from "react";
+import styles from "./styles.module.css";
+import { Radio, Switch } from "antd";
+import { useTheme } from "@hooks";
+import ThreeCuteBrothers from "../threeCuteBrothers";
+import VisualSortingAlgorithm from "../visualSortingAlgorithm";
+import PeriodicTable from "../periodicTable";
+import GlowingBall from "../glowingBall";
+import { GithubOutlined } from "@ant-design/icons";
 
-type TEType = 'ThreeCuteBrothers' | 'VisualSortingAlgorithm' | 'PeriodicTable' | 'GlowingBall';
+type TEType =
+  | "VisualSortingAlgorithm"
+  | "ThreeCuteBrothers"
+  | "PeriodicTable"
+  | "GlowingBall";
 
 const Layout = () => {
-  const cacheTEType = localStorage.getItem('TEType') || 'ThreeCuteBrothers';
+  const cacheTEType =
+    localStorage.getItem("TEType") || "VisualSortingAlgorithm";
   const [curTEType, setCurTEType] = useState<TEType>(cacheTEType as TEType);
   const [theme, switchTheme] = useTheme();
-  
-  const tETypeKey = ['ThreeCuteBrothers', 'VisualSortingAlgorithm', 'PeriodicTable', 'GlowingBall'];
-  const tETypeName = ['萌三兄弟', '可视化排序算法', '元素周期表', '发光小球'];
 
-  const _ThreeCuteBrothers = useMemo(() => <ThreeCuteBrothers />, []);
+  const tETypeKey = [
+    "VisualSortingAlgorithm",
+    "ThreeCuteBrothers",
+    "PeriodicTable",
+    "GlowingBall",
+  ];
+  const tETypeName = ["可视化排序算法", "萌三兄弟", "元素周期表", "发光小球"];
+
   const _VisualSortingAlgorithm = useMemo(() => <VisualSortingAlgorithm />, []);
+  const _ThreeCuteBrothers = useMemo(() => <ThreeCuteBrothers />, []);
   const _PeriodicTable = useMemo(() => <PeriodicTable />, []);
   const _MaterialsVariations = useMemo(() => <GlowingBall />, []);
 
   const getComponents = (curTEType: TEType): ReactNode => {
     const components = {
-      'ThreeCuteBrothers': _ThreeCuteBrothers,
-      'VisualSortingAlgorithm': _VisualSortingAlgorithm,
-      'PeriodicTable': _PeriodicTable,
-      'GlowingBall': _MaterialsVariations
-    }
+      VisualSortingAlgorithm: _VisualSortingAlgorithm,
+      ThreeCuteBrothers: _ThreeCuteBrothers,
+      PeriodicTable: _PeriodicTable,
+      GlowingBall: _MaterialsVariations,
+    };
     return components[curTEType];
-  }
+  };
 
   const onChange = (e: any) => {
     setCurTEType(e.target.value);
-    localStorage.setItem('TEType', e.target.value);
+    localStorage.setItem("TEType", e.target.value);
   };
 
   return (
     <>
       <div className={styles.title}>
-        <Radio.Group onChange={onChange} value={curTEType} >
-          { 
-            tETypeKey.map((item, index) =>
-              <Radio 
-                key={item}
-                value={item}
-                className={styles.radio}
-              >
-                {tETypeName[index]}
-              </Radio>
-            ) 
-          }
+        <Radio.Group onChange={onChange} value={curTEType}>
+          {tETypeKey.map((item, index) => (
+            <Radio key={item} value={item} className={styles.radio}>
+              {tETypeName[index]}
+            </Radio>
+          ))}
         </Radio.Group>
-        <Switch
-          checkedChildren="🌞"
-          unCheckedChildren="🌙"
-          checked={theme === 'light'}
-          onChange={switchTheme}
-        />
+        <div className={styles.right}>
+          <a
+            target="_blank"
+            rel="noreferrer"
+            className={styles.linkA}
+            href="https://github.com/JacksonGao188/three-js-example-demo.git"
+          >
+            <GithubOutlined style={{ fontSize: "20px" }} />
+          </a>
+          <Switch
+            checkedChildren="🌞"
+            unCheckedChildren="🌙"
+            checked={theme === "light"}
+            onChange={switchTheme}
+          />
+        </div>
       </div>
-      <div className={styles.container}>
-        { getComponents(curTEType) }
-      </div>
+      <div className={styles.container}>{getComponents(curTEType)}</div>
     </>
-  )
-}
+  );
+};
 
 export default Layout;
